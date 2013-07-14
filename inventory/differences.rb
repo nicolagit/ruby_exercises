638b9f0bc218f2e8077e7f3d082355dbf2628e35
Nicola <nyc.zyp@gmail.com>
2013-07-14 20:15:50 +0100

    3.9 Exercises 3

--- inventory/differences.rb
+++ inventory/differences.rb
@@ -9,3 +9,17 @@ puts ""
 puts "The following files have been deleted:"
 puts old_inventory - new_inventory 
 
+puts ""
+
+added_files = (new_inventory - old_inventory)
+puts added_files.length
+puts "new files have been added to the new inventory"
+
+removed_files = (old_inventory - new_inventory)
+puts removed_files.length
+puts "old files have been removed from the old inventory"
+
+puts ""
+
+puts new_inventory.length - added_files.length 
+puts "files were not changed"
