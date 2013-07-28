4d79c54680b8a63d18892e0b6bf17aadecfed2f0
Nicola <nyc.zyp@gmail.com>
2013-07-28 18:31:38 +0100

    5.7

--- inventory/differences.rb
+++ inventory/differences.rb
@@ -1,6 +1,8 @@
-unless ARGV.length == 2
-  puts "Usage: differences.rb old-inventory new-inventory"
-  exit
+def input_check
+  unless ARGV.length == 2
+    puts "Usage: differences.rb old-inventory new-inventory"
+    exit
+  end
 end
 
 def boring_word_detected?(line)
@@ -18,28 +20,36 @@ def load_file(input_file)
   end
 end
 
-old_inventory = load_file(ARGV[0])
-new_inventory = load_file(ARGV[1])
+def compare_inventory_files(old_file, new_file)
+  old_inventory = load_file(old_file)
+  new_inventory = load_file(new_file)
 
-puts "The following files have been added:"
-puts new_inventory - old_inventory
+  puts "The following files have been added:"
+  puts new_inventory - old_inventory
 
-puts ""
+  puts ""
 
-puts "The following files have been deleted:"
-puts old_inventory - new_inventory 
+  puts "The following files have been deleted:"
+  puts old_inventory - new_inventory 
 
-puts ""
+  puts ""
 
-added_files = (new_inventory - old_inventory)
-puts added_files.length
-puts "new files have been added to the new inventory"
+  added_files = (new_inventory - old_inventory)
+  puts added_files.length
+  puts "new files have been added to the new inventory"
 
-removed_files = (old_inventory - new_inventory)
-puts removed_files.length
-puts "old files have been removed from the old inventory"
+  removed_files = (old_inventory - new_inventory)
+  puts removed_files.length
+  puts "old files have been removed from the old inventory"
 
-puts ""
+  puts ""
 
-puts new_inventory.length - added_files.length 
-puts "files were not changed"
+  puts new_inventory.length - added_files.length 
+  puts "files were not changed"
+end
+
+if $0 == __FILE__
+  input_check
+  compare_inventory_files(ARGV[0], ARGV[0])
+end
+
