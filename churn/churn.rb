609c10a728396b9aac448ae4ad15295931306daf
Nicola <nyc.zyp@gmail.com>
2013-09-18 09:49:22 +0100

    7.2b

--- churn/churn.rb
+++ churn/churn.rb
@@ -3,6 +3,11 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/bmsft for more book information.
 #---
+
+def month_before(time)
+  time = time - (28 * 24 * 60 * 60)
+end
+
 if $0 == __FILE__    #(1)
   subsystem_names = ['audit', 'fulfillment', 'persistence',    #(2)
                      'ui', 'util', 'inventory']
